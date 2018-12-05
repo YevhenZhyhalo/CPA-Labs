cec47eaf9c814fa09d6ec98762aad1aad1057140
gekailovegeka <gekailovegeka@gmail.com>
2018-12-05 22:56:36 +0200

    my cpa labs

--- 1/1.3.1.2.c
+++ 1/1.3.1.2.c
@@ -1,6 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
 int main()
 {
-printf("The value of six is: %d \n", 6);
-return 0;
+	printf("The value of six is: %d\n", 16 - 10);
+	return 0;
 }
